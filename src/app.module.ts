7d1c84b1860c96851d8566afff483340f3fe2f0b
Mohamed Hassan <mohamed.hassan.pet@gmail.com>
2023-01-04 01:26:27 +0400

    fix: fix file paths

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
-import { AccountModule } from '@modules/account/account.module';
-import { ContractModule } from '@modules/contract/contract.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AccountModule } from './modules/account/account.module';
+import { ContractModule } from './modules/contract/contract.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), ContractModule, AccountModule],
