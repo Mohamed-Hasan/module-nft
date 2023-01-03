922cb038731a0bbc3c99ac3b7aab123bd6011f3d
Mohamed Hassan <mohamed.hassan.pet@gmail.com>
2023-01-04 00:51:08 +0400

    feat: enhance config

--- src/core/config.ts
+++ src/core/config.ts
@@ -2,4 +2,6 @@ import 'dotenv/config';
 
 export const config: Record<string, any> = {
   port: process.env.PORT || 3000,
+  alchemyApiKey: process.env.ALKHEMY_API_KEY,
+  localAppUrl: `http://localhost:${process.env.PORT || 3000}/module-nft`,
 };
